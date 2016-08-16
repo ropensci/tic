85a107e365aaeb1f65e20f66a80b535269eaa0b1
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 23:28:12 +0200

    add basic package documentation

---
+++ R/tic-package.R
@@ -0,0 +1,8 @@
+#' @details
+#' Use \code{\link{github_repo}} to get the name of the current repository
+#' as determined from the \code{origin} remote. The \code{\link{github_info}}
+#' function queries basic information on your repository.
+#' The \code{\link{travis_set_var}} and \code{\link{github_add_key}} functions
+#' interact with the GitHub and Travis configuration of your repository,
+#' and ask for permission the first time you call them.
+"_PACKAGE"
