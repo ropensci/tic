c5c9c40bbcc428ffa2bf1d25f03004a1d47e562b
Mika Braginsky <mikabr@stanford.edu>
2016-09-07 12:29:19 -0400

    first pass modularization

--- R/deploy.R
+++ R/deploy.R
@@ -1,5 +1,5 @@
 #' @export
-deploy <- function() {
+deploy <- function(tasks) {
 
   # get system environment variables
   env <- Sys.getenv(c("TRAVIS_BRANCH", "TRAVIS_EVENT_TYPE", "TRAVIS_REPO_SLUG",
@@ -11,55 +11,9 @@ deploy <- function() {
 
   # only run on pushes to master
   if (env[["TRAVIS_BRANCH"]] == "master" && env[["TRAVIS_EVENT_TYPE"]] == "push") {
-
-    # clone repo
-    # TODO: is cloning repo into already cloned repo bad?
-    repo_url <- sprintf("git@github.com:%s.git", env[["TRAVIS_REPO_SLUG"]])
-    local_path <- env[["TRAVIS_REPO_SLUG"]]
-    system(sprintf("git clone --depth=50 %s %s", repo_url, local_path),
-           ignore.stdout = TRUE, ignore.stderr = TRUE)
-    repo <- git2r::repository(local_path)
-    # repo <- git2r::clone(repo_url, local_path = env[["TRAVIS_REPO_SLUG"]],
-    #                      branch = "master")
-    setwd(local_path)
-
-    # decrypt deploy key
-    deploy_key <- openssl::aes_cbc_decrypt(
-      ".deploy_key.enc", openssl::base64_decode(env[["encryption_key"]]),
-      openssl::base64_decode(env[["encryption_iv"]])
-    )
-    deploy_key_path <- ".deploy_key"
-    writeBin(deploy_key, deploy_key_path)
-    #cred <- git2r::cred_ssh_key(".deploy_key.pub", ".deploy_key")
-    Sys.chmod(deploy_key_path, "600")
-    system(sprintf("ssh-agent sh -c 'ssh-add %s'", deploy_key_path))
-
-    # configure repo and switch to gh-pages branch
-    author <- git2r::commits(repo)[[1]]@author
-    git2r::config(repo, user.name = author@name, user.email = author@email)
-    git2r::checkout(repo, branch = "gh-pages", create = TRUE)
-
-    # copy over rendered vignettes
-    pkg_name <- unlist(strsplit(env[["RCHECK_DIR"]], ".Rcheck"))
-    src_dir <- sprintf("../../%s/00_pkg_src/%s/inst/doc", env[["RCHECK_DIR"]],
-                       pkg_name)
-    html_files <- list.files(src_dir, pattern = "*.html")
-    file.copy(unlist(lapply(html_files,
-                            function(file) file.path(src_dir, file))),
-              ".", overwrite = TRUE)
-
-    # commit and push
-    git2r::add(repo, "*.html")
-    st <- vapply(git2r::status(repo), length, integer(1))
-    if (st[["staged"]] != 0) {
-      system("git commit -m 'deploy to github pages'")
-      #git2r::commit(repo, message = "deploy to github pages")
-      system("git push -u origin gh-pages")
-      #git2r::push(repo, "origin", "gh-pages", credentials = cred)
+    for (task in tasks) {
+      eval(parse(text = task))
     }
-
-    unlink(".deploy_key")
-
   }
 
 }
--- R/tasks.R
+++ R/tasks.R
@@ -1,41 +1,62 @@
-#' Use travis vignettes
-#'
-#' @param pkg package description, can be path or package name. See
-#'   \code{\link{as.package}} for more information.
-#'
 #' @export
-use_travis_vignettes <- function(pkg = ".") {
-  pkg <- devtools::as.package(pkg)
-  travis_path <- file.path(pkg$path, ".travis.yml")
-  key_file <- ".deploy_key"
-  key_path <- file.path(pkg$path, key_file)
-  pub_key_file <- paste0(key_file, ".pub")
-  pub_key_path <- file.path(pkg$path, pub_key_file)
-  enc_key_file <- paste0(key_file, ".enc")
-  enc_key_path <- file.path(pkg$path, enc_key_file)
+task_test <- function() {
+  print("woof")
+}
 
-  if (!file.exists(travis_path)) devtools::use_travis(pkg)
-  travis_yml <- yaml::yaml.load_file(travis_path)
+#' @export
+task_setup_repo <- function() {
 
-  # authenticate on github and travis and set up keys/vars
-  gh <- github_info(pkg$path)
-  setup_keys(gh$owner$login, gh$name, key_path, pub_key_path, enc_key_path)
-  devtools::use_build_ignore(pub_key_file, pkg = pkg)
-  devtools::use_build_ignore(enc_key_file, pkg = pkg)
+  # clone repo
+  # TODO: is cloning repo into already cloned repo bad?
+  repo_url <- sprintf("git@github.com:%s.git", env[["TRAVIS_REPO_SLUG"]])
+  local_path <- env[["TRAVIS_REPO_SLUG"]]
+  system(sprintf("git clone --depth=50 %s %s", repo_url, local_path),
+         ignore.stdout = TRUE, ignore.stderr = TRUE)
+  repo <- git2r::repository(local_path)
+  # repo <- git2r::clone(repo_url, local_path = env[["TRAVIS_REPO_SLUG"]],
+  #                      branch = "master")
+  setwd(local_path)
 
-  # update .travis.yml
-  new_travis_yml <- edit_travis_yml(travis_yml)
-  writeLines(yaml::as.yaml(new_travis_yml), travis_path)
+  # decrypt deploy key
+  deploy_key <- openssl::aes_cbc_decrypt(
+    ".deploy_key.enc", openssl::base64_decode(env[["encryption_key"]]),
+    openssl::base64_decode(env[["encryption_iv"]])
+  )
+  deploy_key_path <- ".deploy_key"
+  writeBin(deploy_key, deploy_key_path)
+  #cred <- git2r::cred_ssh_key(".deploy_key.pub", ".deploy_key")
+  Sys.chmod(deploy_key_path, "600")
+  system(sprintf("ssh-agent sh -c 'ssh-add %s'", deploy_key_path))
 
-  # commit changes to git
-  r <- git2r::repository(pkg$path)
-  st <- vapply(git2r::status(r), length, integer(1))
-  if (any(st != 0)) {
-    git2r::add(r, ".Rbuildignore")
-    git2r::add(r, ".travis.yml")
-    git2r::add(r, pub_key_file)
-    git2r::add(r, enc_key_file)
-    git2r::commit(r, "set up travis pushing vignettes to gh-pages")
+  # configure repo
+  author <- git2r::commits(repo)[[1]]@author
+  git2r::config(repo, user.name = author@name, user.email = author@email)
+
+}
+
+#' @export
+task_push_vignettes <- function() {
+
+  #switch to gh-pages branch
+  git2r::checkout(repo, branch = "gh-pages", create = TRUE)
+
+  # copy over rendered vignettes
+  pkg_name <- unlist(strsplit(env[["RCHECK_DIR"]], ".Rcheck"))
+  src_dir <- sprintf("../../%s/00_pkg_src/%s/inst/doc", env[["RCHECK_DIR"]],
+                     pkg_name)
+  html_files <- list.files(src_dir, pattern = "*.html")
+  file.copy(unlist(lapply(html_files,
+                          function(file) file.path(src_dir, file))),
+            ".", overwrite = TRUE)
+
+  # commit and push
+  git2r::add(repo, "*.html")
+  st <- vapply(git2r::status(repo), length, integer(1))
+  if (st[["staged"]] != 0) {
+    system("git commit -m 'deploy to github pages'")
+    #git2r::commit(repo, message = "deploy to github pages")
+    system("git push -u origin gh-pages")
+    #git2r::push(repo, "origin", "gh-pages", credentials = cred)
   }
 
 }
