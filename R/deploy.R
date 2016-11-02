8c83dbd4d92bac0c35ef7d4ac985acdfec9f691f
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-02 22:01:48 +0100

    deploying R6 tasks

--- R/deploy.R
+++ R/deploy.R
@@ -1,25 +1,35 @@
 #' @export
-install_deploy_dependencies <- function(task_code = get_task_code()) {
+prepare_deploy <- function(task_code = get_task_code()) {
 
-  parsed <- parse_task_code(task_code)
+  tasks <- parse_task_code(task_code)
 
-  packages <- get_packages_from_parsed(parsed)
-
-  if (length(packages) == 0L) {
-    return(invisible())
-  }
-
-  message("Installing deploy dependencies: ", paste(packages, collapse = ", "))
-  devtools::install_cran(packages)
+  lapply(tasks, function(task) {
+    task_name <- class(task)[[1L]]
+    if (body(task$prepare) != quote({})) {
+      if (!task$check()) {
+        message("Skipping deploy preparation: ", task_name)
+      } else {
+        message("Preparing deploy: ", task_name)
+        task$prepare()
+      }
+    }
+  })
 
 }
 
 #' @export
 deploy <- function(task_code = get_task_code()) {
 
-  parsed <- parse_task_code(task_code)
+  tasks <- parse_task_code(task_code)
 
-  lapply(parsed, eval)
+  lapply(tasks, function(task) {
+    if (!task$check()) {
+      message("Skipping deploy: ", task_name)
+    } else {
+      message("Deploying: ", task_name)
+      task$prepare()
+    }
+  })
 
 }
 
@@ -30,23 +40,5 @@ get_task_code <- function() {
 parse_task_code <- function(task_code) {
   parsed <- as.list(parse(text = task_code))
   names(parsed) <- vapply(parsed, deparse, nlines = 1L, character(1L))
-  parsed
-}
-
-get_packages_from_parsed <- function(parsed) {
-  unlist(lapply(parsed, get_packages_from_parsed_one))
-}
-
-get_packages_from_parsed_one <- function(parsed_one) {
-  task <- as.list(parsed_one)[[1L]]
-
-  if (is.call(task)) {
-    task_call_components <- as.list(task)
-    if (task_call_components[[1L]] == "::") {
-      return(as.character(task_call_components[[2L]]))
-    }
-  }
-
-  task_object <- eval(task)
-  attr(task_object, "packages")
+  lapply(parsed, eval)
 }
