78bedcb2fc778a59e758afc53abe4d8e0c964fc7
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-02 19:25:31 +0100

    implement simple task parser
    from environment variable RTRAVIS_TASKS

--- R/deploy.R
+++ R/deploy.R
@@ -19,5 +19,15 @@ deploy <- function(tasks = get_tasks()) {
 }
 
 get_tasks <- function() {
-  character()
+  parse_task_env_value(Sys.getenv("RTRAVIS_TASKS"))
+}
+
+parse_task_env_value <- function(env_value) {
+  env_value <- paste0(env_value, " ")
+  split <- strsplit(env_value, "[)] +")[[1L]]
+  if (length(split) == 1) {
+    character()
+  } else {
+    paste0(split, ")")
+  }
 }
