c4301576b24d196c54c67557539c91aa9826d078
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-04 13:29:58 +0100

    get key from env var

--- R/tasks-git.R
+++ R/tasks-git.R
@@ -4,16 +4,12 @@ InstallSSHKeys <- R6Class(
   public = list(
     run = function() {
       message("Decrypting deploy key")
-      deploy_key <- openssl::aes_cbc_decrypt(
-        ".deploy_key.enc", openssl::base64_decode(Sys.getenv("encryption_key")),
-        openssl::base64_decode(Sys.getenv("encryption_iv"))
-      )
       deploy_key_path <- file.path("~/.ssh", "id_rsa")
       if (file.exists(deploy_key_path)) {
         stop("Not overwriting key", call. = FALSE)
       }
       message("Writing deploy key to ", deploy_key_path)
-      writeBin(deploy_key, deploy_key_path)
+      writeLines(Sys.getenv("id_rsa"), deploy_key_path)
       Sys.chmod(deploy_key_path, "600")
     },
 
