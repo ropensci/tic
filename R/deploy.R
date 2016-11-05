939e2768e9b7b6e170d395d33ae060bb162dd261
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-05 07:28:47 +0100

    deploy and after_script

--- R/deploy.R
+++ R/deploy.R
@@ -1,6 +1,5 @@
 #' @export
-prepare_deploy <- function(task_code = get_task_code()) {
-
+prepare <- function(task_code = c(get_deploy_task_code(), get_after_success_task_code())) {
   tasks <- parse_task_code(task_code)
 
   lapply(tasks, function(task) {
@@ -8,7 +7,7 @@ prepare_deploy <- function(task_code = get_task_code()) {
     # prepare() method overridden?
     if (!identical(task$prepare, TravisTask$public_methods$prepare)) {
       if (!task$check()) {
-        message("Skipping deploy preparation: ", task_name)
+        message("Skipping ", step, " preparation: ", task_name)
       } else {
         message("Preparing deploy: ", task_name)
         task$prepare()
@@ -19,28 +18,46 @@ prepare_deploy <- function(task_code = get_task_code()) {
 }
 
 #' @export
-deploy <- function(task_code = get_task_code()) {
+deploy <- function(task_code = get_deploy_task_code()) {
+  run("deploy", task_code)
+}
 
+#' @export
+after_success <- function(task_code = get_after_success_task_code()) {
+  run("after_success", task_code)
+}
+
+run <- function(step, task_code) {
   tasks <- parse_task_code(task_code)
 
   lapply(tasks, function(task) {
     task_name <- class(task)[[1L]]
     if (!task$check()) {
-      message("Skipping deploy: ", task_name)
+      message("Skipping ", step, ": ", task_name)
     } else {
-      message("Deploying: ", task_name)
+      message("Running ", step, ": ", task_name)
       task$run()
     }
   })
 
 }
 
-get_task_code <- function() {
-  Sys.getenv("RTRAVIS_TASKS")
+#' @export
+get_deploy_task_code <- function() {
+  Sys.getenv("RTRAVIS_DEPLOY_TASKS")
+}
+
+#' @export
+get_after_success_task_code <- function() {
+  Sys.getenv("RTRAVIS_AFTER_success_TASKS")
 }
 
 parse_task_code <- function(task_code) {
-  parsed <- as.list(parse(text = task_code))
+  parsed <- unlist(lapply(task_code, parse_one))
   names(parsed) <- vapply(parsed, deparse, nlines = 1L, character(1L))
   lapply(parsed, eval)
+}
+
+parse_one <- function(code) {
+  as.list(parse(text = code))
 }
