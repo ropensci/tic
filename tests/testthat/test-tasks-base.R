28b5702ad48a55dc5b698026946b1c2ea1ae66e9
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-05 16:24:18 +0100

    base tests

---
+++ tests/testthat/test-tasks-base.R
@@ -0,0 +1,15 @@
+context("tasks-base")
+
+test_that("can't run base task", {
+  task <- TravisTask$new()
+  expect_null(task$prepare())
+  expect_true(task$check())
+  expect_error(task$run(), "override")
+})
+
+test_that("can run hello world task", {
+  task <- task_hello_world()
+  expect_null(task$prepare())
+  expect_true(task$check())
+  expect_output(task$run(), "world")
+})
