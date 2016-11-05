a94c2a4497f71444962e3c1df755aeb0b731966f
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-05 22:24:38 +0100

    steps

--- R/deploy.R
+++ R/deploy.R
@@ -1,7 +1,7 @@
 #' @export
-before_script <- function(task_code = c(get_deploy_task_code(), get_after_success_task_code())) {
+before_script <- function(steps = c(get_deploy_steps(), get_after_success_steps())) {
 
-  tasks <- parse_task_code(task_code)
+  tasks <- parse_steps(steps)
   exec_before_script(tasks)
 
 }
@@ -27,63 +27,90 @@ exec_before_script <- function(tasks) {
 }
 
 #' @export
-deploy <- function(task_code = get_deploy_task_code()) {
-  run("deploy", task_code)
+deploy <- function(steps = get_deploy_steps()) {
+  run("deploy", steps)
 }
 
 #' @export
-after_success <- function(task_code = get_after_success_task_code()) {
-  run("after_success", task_code)
+after_success <- function(steps = get_after_success_steps()) {
+  run("after_success", steps)
 }
 
-run <- function(step, task_code) {
+run <- function(stage, steps) {
 
-  tasks <- parse_task_code(task_code)
-  exec_run(step, tasks)
+  tasks <- parse_steps(steps)
+  exec_run(stage, tasks)
 
 }
 
-exec_run <- function(step, tasks) {
+exec_run <- function(stage, tasks) {
 
-  check_results <- call_check(tasks, step)
+  check_results <- call_check(tasks, stage)
 
   lapply(tasks[check_results], function(task) {
     task_name <- class(task)[[1L]]
-    message("Running ", step, ": ", task_name)
+    message("Running ", stage, ": ", task_name)
     task$run()
   })
 
 }
 
 #' @export
-get_after_success_task_code <- function() {
+get_after_success_steps <- function() {
   Sys.getenv("TIC_AFTER_SUCCESS_TASKS")
 }
 
 #' @export
-get_deploy_task_code <- function() {
+get_deploy_steps <- function() {
   Sys.getenv("TIC_DEPLOY_TASKS")
 }
 
-parse_task_code <- function(task_code) {
-  parsed <- Reduce(c, lapply(task_code, parse_one), list())
-  names(parsed) <- vapply(parsed, deparse, nlines = 1L, character(1L))
-  eval_result <- lapply(parsed, eval, asNamespace(utils::packageName()))
-  funcs <- vlapply(eval_result, is.function)
-  eval_result[funcs] <- lapply(eval_result[funcs], do.call, args = list())
-  eval_result
+parse_steps <- function(steps) {
+  steps <- coerce_steps(steps)
+  valid_steps <- get_valid_steps(steps)
+  tasks <- create_tasks(valid_steps)
+  tasks
 }
 
-parse_one <- function(code) {
-  as.list(parse(text = code))
+coerce_steps <- function(steps) {
+  steps
 }
 
-call_check <- function(tasks, action) {
+get_valid_steps <- function(steps) {
+  correct_branch <- get_correct_branch(steps)
+  env_set <- get_env_set(steps)
+  steps[correct_branch & env_set]
+}
+
+get_correct_branch <- function(steps) {
+  on_branch <- lapply(steps, "[[", "on_branch")
+  vlapply(on_branch, match_branch)
+}
+
+get_env_set <- function(steps) {
+  on_env <- lapply(steps, "[[", "on_env")
+  is_null <- vlapply(on_env, is.null)
+  ret <- rep(TRUE, length(on_env))
+  ret[!is_null] <- (vcapply(on_env[!is_null], Sys.getenv) != "")
+  ret
+}
+
+create_tasks <- function(steps) {
+  tasks <- lapply(steps, create_task)
+  names(tasks) <- vcapply(lapply(tasks, class), "[[", 1L)
+  tasks
+}
+
+create_task <- function(step) {
+  do.call(step$task, step$args)
+}
+
+call_check <- function(tasks, stage) {
   checks <- lapply(tasks, "[[", "check")
   check_results <- vlapply(checks, do.call, args = list())
 
   if (any(!check_results)) {
-    message("Skipping ", action, ":")
+    message("Skipping ", stage, ":")
     print(lapply(checks[!check_results], body))
   }
 
