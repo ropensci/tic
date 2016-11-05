c50047062a99afc3ccb9e7b6fe59458bd9735eb1
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-05 16:35:02 +0100

    AppVeyorCI

---
+++ R/appveyor.R
@@ -0,0 +1,11 @@
+#' @include ci.R
+AppVeyorCI <- R6Class(
+  "AppVeyorCI",
+  inherit = CI,
+
+  public = list(
+    get_branch = function() {
+      Sys.getenv("APPVEYOR_REPO_BRANCH")
+    }
+  )
+)
