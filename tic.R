03c7577253af99c3183eb10ab91cc70af0d3617f
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-16 11:16:52 +0100

    model new DSL

--- tic.R
+++ tic.R
@@ -1,10 +1,13 @@
-after_success <- list(
-  step(task_hello_world),
-  step(task_run_covr)
-)
+stage("after_success") %>%
+  add_step(task_hello_world) %>%
+  add_step(task_run_covr)
 
-deploy <- list(
-  step(task_build_pkgdown, on_branch = "production", on_env = "BUILD_PKGDOWN"),
-  step(task_test_ssh),
-  step(task_push_deploy, path = "docs", branch = "gh-pages", on_branch = "production", on_env = "BUILD_PKGDOWN")
-)
+stage("deploy") %>%
+  add_step(task_install_ssh_keys) %>%
+  add_step(task_test_ssh)
+
+if (ci()$get_branch() == "production" && Sys.getenv("BUID_PKGDOWN") != "") {
+  stage("deploy") %>%
+    add_step(task_build_pkgdown) %>%
+    add_step(task_push_deploy, path = "docs", branch = "gh-pages")
+}
