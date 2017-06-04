a3d42d5358272a8ae315a72c0fe8af851a4e75d4
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-05 01:35:39 +0200

    magenta, not inverse

--- R/stage.R
+++ R/stage.R
@@ -63,7 +63,7 @@ Stage <- R6Class(
       }
 
       ci()$cat_with_color(
-        crayon::inverse(paste0("Running ", private$name, ": ", step$name))
+        crayon::magenta(paste0("Running ", private$name, ": ", step$name))
       )
       step$run()
     }
