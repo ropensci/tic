15f76153f3f4de84cf67d5cd454166404c54f3bb
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-26 00:59:39 +0200

    new step_setup_push_deploy() and step_do_push_deploy()
    - Split `step_push_deploy()` into `step_setup_push_deploy()` and `step_do_push_deploy()`.

--- R/steps-git.R
+++ R/steps-git.R
@@ -87,55 +87,76 @@ TestSSH <- R6Class(
 #' @export
 step_test_ssh <- TestSSH$new
 
-PushDeploy <- R6Class(
-  "PushDeploy", inherit = TicStep,
+Git <- R6Class(
+  "Git",
 
   public = list(
-    initialize = function(path = ".", branch = ci()$get_branch(), orphan = FALSE,
-                          remote_url = paste0("git@github.com:", ci()$get_slug(), ".git"),
-                          commit_message = NULL) {
-      if (branch == ci()$get_branch() && orphan) {
-        stop("Cannot orphan the branch that has been used for the CI run.", call. = FALSE)
-      }
-
+    initialize = function(path) {
       private$path <- path
-      private$branch <- branch
-      private$orphan <- orphan
-      private$remote_url <- remote_url
-      if (is.null(commit_message)) {
-        commit_message <- private$format_commit_message()
+    },
+
+    cmd = function(...) {
+      args <- c(...)
+      message(paste("git", paste(args, collapse = " ")))
+      status <- withr::with_dir(private$path, system2("git", args))
+      if (status != 0) {
+        stopc("git exited with status ", status)
       }
-      private$commit_message <- commit_message
     },
 
-    prepare = function() {
-      private$init_repo()
-      private$init_author()
-      private$fetch()
+    init_repo = function() {
+      message("Initializing Git repo at ", private$path)
+      dir.create(private$path, recursive = TRUE, showWarnings = FALSE)
+      private$repo <- git2r::init(private$path)
     },
 
-    run = function() {
-      private$init_repo()
-      private$commit()
-      private$push()
+    get_repo = function() {
+      private$repo
     }
   ),
 
   private = list(
     path = NULL,
+    repo = NULL
+  )
+)
+
+SetupPushDeploy <- R6Class(
+  "SetupPushDeploy", inherit = TicStep,
+
+  public = list(
+    initialize = function(path = ".", branch = ci()$get_branch(), orphan = FALSE,
+                          remote_url = paste0("git@github.com:", ci()$get_slug(), ".git"),
+                          checkout = TRUE) {
+
+      if (branch == ci()$get_branch() && orphan) {
+        stop("Cannot orphan the branch that has been used for the CI run.", call. = FALSE)
+      }
+
+      private$git <- Git$new(path)
+      private$branch <- branch
+      private$orphan <- orphan
+      private$remote_url <- remote_url
+      private$checkout <- checkout
+    },
+
+    run = function() {
+      private$git$init_repo()
+      private$init_author()
+      private$fetch()
+    }
+  ),
+
+  private = list(
+    git = NULL,
+
     branch = NULL,
     orphan = FALSE,
     remote_url = NULL,
-    commit_message = NULL,
+    checkout = FALSE,
 
     repo = NULL,
-    remote_name = "origin",
-
-    init_repo = function() {
-      message("Initializing Git repo at ", private$path)
-      dir.create(private$path, recursive = TRUE)
-      private$repo <- git2r::init(private$path)
-    },
+    remote_name = "origin", # HACK
 
     init_author = function() {
       latest_commit <- get_head_commit(git2r::head(git2r::repository(".")))
@@ -144,24 +165,38 @@ PushDeploy <- R6Class(
       latest_author <- latest_commit@author
       print(latest_author)
 
-      git2r::config(private$repo, user.name = latest_author@name, user.email = latest_author@email)
+      git2r::config(private$git$get_repo(), user.name = latest_author@name, user.email = latest_author@email)
     },
 
     fetch = function() {
       remote_name <- private$remote_name
-      message("Fetching from remote ", remote_name)
 
-      if (remote_name %in% git2r::remotes(private$repo)) {
-        git2r::remote_remove(private$repo, remote_name)
+      if (remote_name %in% git2r::remotes(private$git$get_repo())) {
+        message("Not overriding existing remote ", remote_name)
+      } else {
+        message("Adding remote ", remote_name, " with URL ", private$remote_url)
+        git2r::remote_add(private$git$get_repo(), remote_name, private$remote_url)
       }
-      git2r::remote_add(private$repo, remote_name, private$remote_url)
+
+      message("Setting branch name to ", private$branch)
+      private$git$cmd("checkout", "-B", private$branch)
 
       if (!private$orphan) {
+        message("Fetching from remote ", remote_name)
         tryCatch(
           {
             remote_branch <- private$try_fetch()
             if (!is.null(remote_branch)) {
-              git2r::reset(get_head_commit(remote_branch))
+              if (private$checkout) {
+                git2r::checkout(
+                  private$git$get_repo(),
+                  private$branch,
+                  create = TRUE,
+                  force = TRUE
+                )
+              } else {
+                git2r::reset(get_head_commit(remote_branch))
+              }
             }
           },
           error = function(e) {
@@ -174,35 +209,66 @@ PushDeploy <- R6Class(
 
     try_fetch = function() {
       remote_name <- private$remote_name
-      private$git("fetch", remote_name, paste0("refs/heads/", private$branch))
-      branches <- git2r::branches(private$repo, "remote")
+      private$git$cmd("fetch", remote_name, paste0("refs/heads/", private$branch))
+      branches <- git2r::branches(private$git$get_repo(), "remote")
       branches[[paste0(remote_name, "/", private$branch)]]
+    }
+
+  )
+)
+
+#' @export
+step_setup_push_deploy <- SetupPushDeploy$new
+
+DoPushDeploy <- R6Class(
+  "PushDeploy", inherit = TicStep,
+
+  public = list(
+    initialize = function(path = ".", commit_message = NULL) {
+      private$git <- Git$new(path)
+
+      if (is.null(commit_message)) {
+        commit_message <- private$format_commit_message()
+      }
+      private$commit_message <- commit_message
     },
+
+    run = function() {
+      private$git$init_repo()
+      maybe_orphan <- is.null(git2r::head(private$git$get_repo()))
+      if (private$commit()) private$push(force = maybe_orphan)
+    }
+  ),
+
+  private = list(
+    git = NULL,
+
+    commit_message = NULL,
+
+    repo = NULL,
+    remote_name = "origin", # HACK
 
     commit = function() {
-      message("Committing to ", private$path)
-      git2r::add(private$repo, ".")
-      status <- git2r::status(private$repo, staged = TRUE, unstaged = FALSE, untracked = FALSE, ignored = FALSE)
+      message("Committing to ", private$git$get_repo()@path)
+      git2r::add(private$git$get_repo(), ".")
+      status <- git2r::status(private$git$get_repo(), staged = TRUE, unstaged = FALSE, untracked = FALSE, ignored = FALSE)
       if (length(status$staged) > 0) {
-        git2r::commit(private$repo, private$commit_message)
+        git2r::commit(private$git$get_repo(), private$commit_message)
+        TRUE
       } else {
         message("Nothing to commit!")
+        FALSE
       }
     },
 
-    push = function() {
+    push = function(force) {
       message("Pushing to remote")
-      private$git("push", if (private$orphan) "--force", private$remote_name,
-                  paste0("HEAD:", private$branch))
-    },
-
-    git = function(...) {
-      args <- c(...)
-      message(paste("git", paste(args, collapse = " ")))
-      status <- withr::with_dir(private$path, system2("git", args))
-      if (status != 0) {
-        stopc("git exited with status ", status)
-      }
+      private$git$cmd(
+        "push",
+        if (force) "--force",
+        private$remote_name,
+        "HEAD"
+      )
     },
 
     format_commit_message = function() {
@@ -212,6 +278,39 @@ PushDeploy <- R6Class(
         "Commit: ", ci()$get_commit()
       )
     }
+  )
+)
+
+#' @export
+step_do_push_deploy <- DoPushDeploy$new
+
+PushDeploy <- R6Class(
+  "PushDeploy", inherit = TicStep,
+
+  public = list(
+    initialize = function(path = ".", branch = ci()$get_branch(), orphan = FALSE,
+                          remote_url = paste0("git@github.com:", ci()$get_slug(), ".git"),
+                          commit_message = NULL) {
+
+      private$setup <- step_setup_push_deploy(
+        path = path, branch = branch, orphan = orphan, remote_url = remote_url
+      )
+
+      private$do <- step_do_push_deploy(
+        path = path, commit_message = commit_message
+      )
+
+    },
+
+    run = function() {
+      private$setup$run()
+      private$do$run()
+    }
+  ),
+
+  private = list(
+    setup <- NULL,
+    do <- NULL
   )
 )
 
