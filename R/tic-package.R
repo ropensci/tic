f1a4eb60f2597cdf14d9711a1e2872bc09eb200d
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-29 01:39:51 +0200

    package docs

--- R/tic-package.R
+++ R/tic-package.R
@@ -1,8 +1,6 @@
 #' @details
-#' Use \code{\link{github_repo}} to get the name of the current repository
-#' as determined from the \code{origin} remote. The \code{\link{github_info}}
-#' function queries basic information on your repository.
-#' The \code{\link{travis_set_var}} and \code{\link{github_add_key}} functions
-#' interact with the GitHub and Travis configuration of your repository,
-#' and ask for permission the first time you call them.
+#' The [travis::use_tic()] function prepares a code repository for use with
+#' this package. See [DSL] for an overview of \pkg{tic}'s domain-specific
+#' language for defining stages and steps, and [step_hello_world()] and the
+#' links therein for available steps.
 "_PACKAGE"
