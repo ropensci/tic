34c9b59d89ca95dbf06366f10f43f2704619ac23
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-28 19:44:46 +0200

    knit overview
    - `step_add_to_drat()` will also update the overview page.

--- R/steps-drat.R
+++ R/steps-drat.R
@@ -3,13 +3,17 @@ AddToDrat <- R6Class(
 
   public = list(
     prepare = function() {
-      verify_install("drat", "remotes")
+      verify_install("drat", "remotes", "knitr", "withr")
       remotes::install_github("r-lib/pkgbuild")
     },
 
     run = function() {
       path <- pkgbuild::build(binary = (getOption("pkgType") != "source"))
       drat::insertPackage(path)
+      withr::with_dir(
+        "~/git/drat",
+        knitr::knit("index.Rmd", "index.md")
+      )
     }
   )
 )
