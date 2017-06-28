dbd6afe8cc2554d1a53b33b8c83165a8cc2db6a3
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-29 01:47:40 +0200

    harmonize .travis.yml and tic.R

--- tic.R
+++ tic.R
@@ -1,14 +1,27 @@
-get_stage("after_success") %>%
-  add_step(step_hello_world()) %>%
-  add_step(step_run_code(covr::codecov()))
+get_stage("before_install") %>%
+  add_step(step_run_code(update.packages(ask = FALSE)))
 
-get_stage("deploy") %>%
-  add_step(step_install_ssh_keys()) %>%
-  add_step(step_add_to_known_hosts("github.com")) %>%
-  add_step(step_test_ssh())
+get_stage("install") %>%
+  add_step(step_run_code(remotes::install_deps(dependencies = TRUE)))
+
+get_stage("script") %>%
+  add_step(step_rcmdcheck())
+
+get_stage("after_success") %>%
+  add_step(step_run_code(covr::codecov(quiet = FALSE)))
 
 if (ci()$is_tag() && Sys.getenv("BUILD_PKGDOWN") != "") {
+  # pkgdown documentation can be built optionally. Other example criteria:
+  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+  # - `ci()$is_tag()`: Only for tags, not for branches
+  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+  get_stage("before_deploy") %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_test_ssh())
+
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+    add_step(step_push_deploy())
 }
