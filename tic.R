28bf2d129647f6c92c917ac2831a5e5a032e5826
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-29 02:34:20 +0200

    add new step for testing

--- tic.R
+++ tic.R
@@ -10,17 +10,15 @@ get_stage("script") %>%
 get_stage("after_success") %>%
   add_step(step_run_code(covr::codecov(quiet = FALSE)))
 
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh())
+
 if (ci()$is_tag() && Sys.getenv("BUILD_PKGDOWN") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `ci()$is_tag()`: Only for tags, not for branches
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_add_to_known_hosts("github.com")) %>%
-    add_step(step_test_ssh())
-
   get_stage("deploy") %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy())
