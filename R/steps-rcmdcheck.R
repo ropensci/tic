480abea86dee1b865e6a267f8eebb04f77ccc2cb
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-07 10:11:09 +0100

    print a summary of rcmdcheck output
    - `step_rcmdcheck()` now prints a summary, which also shows e.g. details on installation failures.

--- R/steps-rcmdcheck.R
+++ R/steps-rcmdcheck.R
@@ -11,6 +11,7 @@ RCMDcheck <- R6Class(
 
     run = function() {
       res <- rcmdcheck::rcmdcheck(args = private$args)
+      print(summary(res))
       if (length(res$errors) > 0) {
         stopc("Errors found.")
       }
