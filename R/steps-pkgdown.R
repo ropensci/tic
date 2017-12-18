303bf8709cf8b4275d4b4e0111e1067a39ace0b1
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-18 16:01:08 +0100

    support passing arguments to pkgdown::build_site()

--- R/steps-pkgdown.R
+++ R/steps-pkgdown.R
@@ -2,10 +2,14 @@ BuildPkgdown <- R6Class(
   "BuildPkgdown", inherit = TicStep,
 
   public = list(
+    initialize = function(...) {
+      private$pkgdown_args <- list(...)
+    },
+
     run = function() {
       withr::with_temp_libpaths({
         remotes::install_local(".")
-        pkgdown::build_site(preview = FALSE)
+        do.call(pkgdown::build_site, c(list(preview = FALSE), private$pkgdown_args))
       })
     },
 
@@ -13,6 +17,10 @@ BuildPkgdown <- R6Class(
       verify_install("remotes")
       remotes::install_github("hadley/pkgdown")
     }
+  ),
+
+  private = list(
+    pkgdown_args = NULL
   )
 )
 
@@ -20,8 +28,9 @@ BuildPkgdown <- R6Class(
 #'
 #' Builds package documentation with the \pkg{pkgdown} package.
 #'
+#' @param ... Passed on to [pkgdown::build_site()]
 #' @family steps
 #' @export
-step_build_pkgdown <- function() {
-  BuildPkgdown$new()
+step_build_pkgdown <- function(...) {
+  BuildPkgdown$new(...)
 }
