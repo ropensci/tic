53e9b65a7150b8a3a5332c4cbcda0b49e268caeb
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-30 19:06:33 +0100

    always deploy

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 add_package_checks()
 
-if (ci()$is_tag() && Sys.getenv("BUILD_PKGDOWN") != "") {
+if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
