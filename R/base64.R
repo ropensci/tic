46f053e379259e0df3c1e9e3ae57e40fce97fe73
Kirill Müller <krlmlr+r@mailbox.org>
2018-02-01 06:51:05 +0100

    base64serialize() and base64unserialize()

---
+++ R/base64.R
@@ -0,0 +1,34 @@
+#' Helpers for converting R objects to strings and back
+#'
+#' @description
+#' `base64serialize()` converts an R object into a string suitable for storing
+#' in an environment variable. Use this function for encoding entire R objects
+#' (such as OAuth tokens) so that they can be used in Travis builds.
+#'
+#' @param x Object to serialize or deserialize
+#' @param compression Passed on as `type` argument to [memCompress()] or
+#'   [memDecomporess()].
+#' @examples
+#' serial <- base64serialize(1:10)
+#' serial
+base64serialize <- function(x, compression = "gzip") {
+  raw <- serialize(x, connection = NULL)
+  compressed <- memCompress(raw, type = compression)
+  encoded <- base64enc::base64encode(compressed)
+  encoded
+}
+
+#' @rdname base64serialize
+#' @description
+#' `base64unserialize()` is the inverse operation to `base64serialize()`.
+#' Use this function in your `tic.R` to access the R object previously encoded
+#' by `base64serialize()`.
+#'
+#' @examples
+#' base64unserialize(serial)
+base64unserialize <- function(x, compression = "gzip") {
+  decoded <- base64enc::base64decode(x)
+  expanded <- memDecompress(decoded, type = compression)
+  ret <- unserialize(expanded)
+  ret
+}
