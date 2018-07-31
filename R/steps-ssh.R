eadf9761ff276b9c35e5ad00e1379c445f234df3
pat-s <patrick.schratz@gmail.com>
2018-07-31 21:23:52 +0200

    move get_public_key and encode_private_key from travis

--- R/steps-ssh.R
+++ R/steps-ssh.R
@@ -195,3 +195,18 @@ step_setup_ssh <- function(name = "id_rsa", host = "github.com",
                            url = paste0("git@", host), verbose = "-v") {
   SetupSSH$new(name = name, host = host, url = url, verbose = verbose)
 }
+
+get_public_key <- function(key) {
+  as.list(key)$pubkey
+}
+
+encode_private_key <- function(key) {
+  conn <- textConnection(NULL, "w")
+  openssl::write_pem(key, conn, password = NULL)
+  private_key <- textConnectionValue(conn)
+  close(conn)
+
+  private_key <- paste(private_key, collapse = "\n")
+
+  openssl::base64_encode(charToRaw(private_key))
+}
