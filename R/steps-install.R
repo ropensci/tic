d69d7256f2616d2c95c48a43d3be2275c452f196
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-11 22:31:45 +0200

    Tweak documentation for step_install_xxx(), CC @pat-s

--- R/steps-install.R
+++ R/steps-install.R
@@ -8,11 +8,8 @@ InstallCRAN <- R6Class(
       private$install_args <- list(...)
     },
     run = function() {
-
       if (length(find.package(private$package, quiet = TRUE)) == 0) {
-
         do.call(install.packages, c(list(pkg = private$package), private$install_args))
-
       } else {
         message(glue::glue("Package '{private$package}' already installed."))
       }
@@ -24,14 +21,21 @@ InstallCRAN <- R6Class(
   )
 )
 
-#' Step: Install packages from Github
+#' Step: Install packages
 #'
-#' Install one or more packages from Github
+#' @description
+#' These steps are useful if your CI run needs packages which are not declared
+#' as dependencies in your `DESCRIPTION`.
+#' Usually you should declare these dependencies, but this may not always be desired.
 #'
-#' @param package Package to install
-#' @param ... Passed on to `base::install.packages()`
+#' A `step_install_cran()` step installs one package from CRAN via [install.packages()],
+#' but only if it's not already installed.
+#'
+#' @param package Package(s) to install
+#' @param ... Passed on to [install.packages()].
 #' @family steps
 #' @export
+#' @name step_install_pkg
 step_install_cran <- function(package = NULL, ...) {
   InstallCRAN$new(package = package, ...)
 }
@@ -63,14 +67,16 @@ InstallGithub <- R6Class(
   )
 )
 
-#' Step: Install packages
-#'
-#' Install a package from CRAN if its not already installed.
+#' @description
+#' A `step_install_github()` step installs one or more packages from GitHub
+#' via [remotes::install_github()], the packages are only installed if their
+#' GitHub version is different from the locally installed version.
 #'
 #' @param repo Package to install in the "user/repo" format.
-#' @param ... Passed on to `remotes::install_github()`.
+#' @param ... Passed on to [remotes::install_github()].
 #' @family steps
 #' @export
+#' @rdname step_install_pkg
 step_install_github <- function(repo = NULL, ...) {
   InstallGithub$new(repo = repo, ...)
 }
