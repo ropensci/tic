1d85a2869f4f64f106bc2abcc86460e65e29c881
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-13 01:13:56 +0200

    No longer avoid pkgdown builds for tags, done by tic now

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@ add_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())
 
-if (Sys.getenv("BUILD_PKGDOWN") != "" && !ci()$is_tag()) {
+if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
