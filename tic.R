686f433628d05b3b8cf7007d1b5b5a78faa17734
Kirill Müller <krlmlr+r@mailbox.org>
2018-08-12 15:06:50 +0200

    Check for R >= 3.2 (and not R > 3.1 which includes 3.1.3)

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-add_package_checks(warnings_are_errors = (getRversion() > "3.1"))
+add_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
 
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())
