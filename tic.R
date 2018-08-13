b073f1ca05716230775fbb27a39a00053f2abe98
Patrick Schratz <patrick.schratz@gmail.com>
2018-08-13 23:31:50 +0200

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,16 +1,15 @@
 add_package_checks(warnings_are_errors = (getRversion() >= "3.2"))
 
-get_stage("deploy") %>%
-  add_step(step_build_pkgdown())
+ # pkgdown documentation can be built optionally. Other example criteria:
+   # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
 
 if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
   get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy())
 }
