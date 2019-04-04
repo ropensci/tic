1c05f9c0e1a06037d2717d255142ef84925400fb
pat-s <patrick.schratz@gmail.com>
2019-04-04 23:41:08 +0200

    test pkgdown macro

--- tic.R
+++ tic.R
@@ -3,14 +3,4 @@ add_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error"
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())
 
-if (Sys.getenv("BUILD_PKGDOWN") != "") {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("deploy") %>%
-    add_step(step_push_deploy())
-}
+do_build_pkgdown()
