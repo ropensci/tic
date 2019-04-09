de42f88c8d1f193da3ce34619c8e5dd02cdaa876
pat-s <patrick.schratz@gmail.com>
2019-04-09 23:23:18 +0200

    upd bookdown template

--- inst/templates/bookdown/tic.R
+++ inst/templates/bookdown/tic.R
@@ -1,14 +1,1 @@
-get_stage("install") %>%
-  add_step(step_install_deps())
-
-get_stage("deploy") %>%
-  add_code_step(bookdown::render_book('index.Rmd', 'bookdown::gitbook'))
-
-if (ci_can_push() && !ci_is_tag()) {
-
-    get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_step(step_push_deploy(path = "_book", branch = "gh-pages"))
-}
+do_build_bookdown(input = "")
