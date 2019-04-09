230ea4081f548e16ea0449e699831bb3376e085c
pat-s <patrick.schratz@gmail.com>
2019-04-09 23:14:13 +0200

    test

--- tic.R
+++ tic.R
@@ -1,4 +1,4 @@
-add_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
+do_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
 
 get_stage("deploy") %>%
   add_step(step_build_pkgdown())
