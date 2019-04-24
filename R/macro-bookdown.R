6049b5ff440acd1088a57e71d09cfb5c36a93da1
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-24 18:25:18 +0200

    build_args -> deploy

--- R/macro-bookdown.R
+++ R/macro-bookdown.R
@@ -9,7 +9,6 @@
 NULL
 
 
-
 #' Build a bookdown book
 #'
 #' @description
@@ -19,12 +18,11 @@ NULL
 #' @inheritParams step_build_bookdown
 #' @inheritParams step_setup_push_deploy
 #' @inheritParams step_do_push_deploy
-#' @param build_only To be removed
 #' @family macros
 #' @export
 #' @importFrom magrittr %>%
 do_bookdown <- function(...,
-                        build_only = FALSE,
+                        deploy = NULL,
                         orphan = FALSE,
                         checkout = TRUE,
                         repos = repo_default(),
@@ -32,22 +30,34 @@ do_bookdown <- function(...,
                         remote_url = NULL,
                         commit_message = NULL, commit_paths = ".") {
 
+  #' @param deploy `[flag]`\cr
+  #'   If `TRUE`, deployment setup is performed before building the bookdown site,
+  #'   and the site is deployed after building it.
+  #'   Set to `FALSE` to skip deployment.
+  if (is.null(deploy)) {
+    #'   By default (if `deploy` is `NULL`), deployment happens
+    #'   if the following conditions are met:
+    #'
+    #'   1. The repo can be pushed to (see [ci_can_push()]).
+    deploy <- ci_can_push()
+
+    #'   2. The `branch` argument is `NULL`
+    #'   (i.e., if the deployment happens to the active branch),
+    #'   or the current branch is `master` (see [ci_get_branch()]).
+    if (deploy && !is.null(branch)) {
+      deploy <- (ci_get_branch() == "master")
+    }
+  }
+
   #' @description
-  #' 1. A [step_install_deps()] in the `"install"` stage, using the
+  #' 1. [step_install_deps()] in the `"install"` stage, using the
   #'    `repos` argument.
   get_stage("install") %>%
     add_step(step_install_deps(repos = repos))
 
-  needs_deploy <- !isTRUE(build_only) && ci_can_push()
-
-  if (isTRUE(needs_deploy)) {
-    ci_cat_with_color("`build_only = TRUE` was set, skipping deployment")
-  } else {
-
-    #' 1. [step_setup_ssh()] in the `"before_deploy"` to setup the upcoming deployment.
-    #' 1. [step_setup_push_deploy()] in the `"before_deploy"` stage.
-    #' 1. [step_build_bookdown()] in the `"deploy"` stage
-    #' 1. [step_do_push_deploy()] in the `"deploy"` stage. By default, the deploy is done to the gh-pages branch.
+  if (isTRUE(deploy)) {
+    #' 1. [step_setup_ssh()] in the `"before_deploy"` to setup the upcoming deployment (if `deploy` is set),
+    #' 1. [step_setup_push_deploy()] in the `"before_deploy"` stage (if `deploy` is set),
     get_stage("before_deploy") %>%
       add_step(step_setup_ssh()) %>%
       add_step(step_setup_push_deploy(
@@ -56,16 +66,18 @@ do_bookdown <- function(...,
       ))
   }
 
-  get_stage("script") %>%
+  #' 1. [step_build_bookdown()] in the `"deploy"` stage, forwarding all `...` arguments.
+  get_stage("deploy") %>%
     add_step(step_build_bookdown(...))
 
-
-  if (isTRUE(needs_deploy)) {
-    ci_cat_with_color("`build_only = TRUE` was set, skipping deployment")
-  } else {
+  #' 1. [step_do_push_deploy()] in the `"deploy"` stage.
+  if (isTRUE(deploy)) {
     get_stage("deploy") %>%
       add_step(step_do_push_deploy(
         path = path, commit_message = commit_message, commit_paths = commit_paths
       ))
   }
+
+  #' @description
+  #' By default, the `_book/` directory is deployed to the `gh-pages` branch, keeping the history.
 }
