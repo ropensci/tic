6ce4349c8b3d57e216b98f857a6e23af6e54e070
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-24 18:13:21 +0200

    Move do_package_checks() to separate file

--- R/dsl.R
+++ R/dsl.R
@@ -104,55 +104,6 @@ add_code_step <- function(stage, call = NULL, prepare_call = NULL) {
 }
 
 #' @description
-#' `do_package_checks()` adds default steps related to package checks
-#' to the `"before_install"`, `"install"`, `"script"` and `"after_success"`
-#' stages:
-#'
-#' @inheritParams step_rcmdcheck
-#' @param codecov `[flag]`\cr Whether to include a step running
-#'   `covr::codecov(quiet = FALSE)` (default: only for non-interactive CI,
-#'   see [ci_is_interactive()]).
-#' @rdname DSL
-#' @export
-#' @importFrom magrittr %>%
-do_package_checks <- function(...,
-                              codecov = !ci_is_interactive(),
-                              warnings_are_errors = NULL,
-                              notes_are_errors = NULL,
-                              args = c("--no-manual", "--as-cran"),
-                              build_args = "--force", error_on = "warning",
-                              repos = repo_default(), timeout = Inf) {
-  #' @description
-  #' 1. [step_install_deps()] in the `"install"` stage, using the
-  #'    `repos` argument.
-  get_stage("install") %>%
-    add_step(
-      step_install_deps(repos = repos)
-    )
-
-  #' 1. [step_rcmdcheck()] in the `"script"` stage, using the
-  #'    `warnings_are_errors`, `notes_are_errors`, `args`, and
-  #'    `build_args` arguments.
-  get_stage("script") %>%
-    add_step(
-      step_rcmdcheck(
-        warnings_are_errors = warnings_are_errors,
-        notes_are_errors = notes_are_errors,
-        args = args,
-        build_args = build_args,
-        error_on = error_on,
-        repos = repos,
-        timeout = timeout
-      )
-    )
-
-  if (isTRUE(codecov)) {
-    #' 1. A call to [covr::codecov()] in the `"after_success"` stage (only if the `codecov` flag is set)
-    get_stage("after_success") %>%
-      add_code_step(covr::codecov(quiet = FALSE))
-  }
-}
-#' @description
 #' `do_pkgdown()` builds and optionally deploys a pkgdown site and adds default steps
 #'   to the `"install"`, `"before_deploy"` and `"deploy"` stages:
 #'
