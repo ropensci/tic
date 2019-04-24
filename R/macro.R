b4e7d7a8921c85c371efe88ae1a5d067edef86d3
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-24 11:44:23 +0200

    Documentation stub

---
+++ R/macro.R
@@ -0,0 +1,9 @@
+#' Macros
+#'
+#' The [DSL] offers a fine-grained interface to the individual stages
+#' of a CI run.
+#' Macros are tic's way of adding several related steps to the relevant
+#' stages.
+#' All macros use the `do_` prefix.
+#' @name macro
+NULL
