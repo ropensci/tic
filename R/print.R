03175a200f3baeb6f1bbdbe641cc35099373c19e
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-26 15:11:12 +0200

    Print invisibly

--- R/print.R
+++ R/print.R
@@ -2,9 +2,11 @@
 #' @export
 print.TicStages <- function(x, ...) {
   lapply(x, print, omit_if_empty = TRUE)
+  invisible(x)
 }
 
 #' @export
 print.TicStage <- function(x, ..., omit_if_empty = FALSE) {
   x$print(omit_if_empty = omit_if_empty)
+  invisible(x)
 }
