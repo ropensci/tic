08335f774916b19439e9f5ab589311febb102c44
Kirill Müller <krlmlr+r@mailbox.org>
2019-04-26 11:54:02 +0200

    Use rotemplate
    - Documentation uses the {rotemplate} package (#121).
    
    Closes #121.

--- tic.R
+++ tic.R
@@ -1,3 +1,6 @@
+get_stage("install") %>%
+  add_step(step_install_github("ropensci/rotemplate"))
+
 do_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
 
 if (ci_has_env("BUILD_PKGDOWN")) {
