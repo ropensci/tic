5fd35cd9b77f3a17c53e57b9fd1fd55c955ceac6
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-23 12:10:47 +0200

    Keep empty commits

--- R/steps-git.R
+++ R/steps-git.R
@@ -259,7 +259,7 @@ DoPushDeploy <- R6Class(
       private$git$cmd("clean -fdx")
 
       message("Pulling new changes")
-      private$git$cmd("pull --rebase -X theirs")
+      private$git$cmd("pull --rebase -X theirs --keep-empty")
 
       c_local <- git2r::lookup(private$git$get_repo(), git2r::branch_target(local))
       c_upstream <- git2r::lookup(private$git$get_repo(), git2r::branch_target(upstream))
