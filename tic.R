f481f3a2d6c13b3b76de0815b6f62e11bfa48ab4
pat-s <patrick.schratz@gmail.com>
2019-09-22 17:31:20 +0200

    disable tests on appveyor

--- tic.R
+++ tic.R
@@ -1,7 +1,11 @@
 get_stage("install") %>%
   add_step(step_install_github("ropensci/rotemplate"))
 
-do_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
+if (ci_on_travis()) {
+  do_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
+} else if (ci_on_appveyor()) {
+  do_package_checks(args = "--no-tests", error_on = if (getRversion() >= "3.2") "warning" else "error")
+}
 
 if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown()
