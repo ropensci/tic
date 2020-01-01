5b7690ad69b612c8bd760d92327148c044c7bde2
Kirill Müller <krlmlr+r@mailbox.org>
2020-01-01 12:52:01 +0100

    Add arguments to use_tic()

--- R/use_tic.R
+++ R/use_tic.R
@@ -11,9 +11,38 @@
 #' 1. Create a default `tic.R` file depending on the repo type
 #'    (package, website, bookdown, ...)
 #'
+#' @param wizard `[flag]`\cr Interactive operation? If `TRUE`,
+#'   a menu will be shown, and the `linux`, `mac`, `windows`,
+#'   `deploy` and `matrix` arguments are ignored.
+#' @param linux,windows,mac `[string]`\cr Which CI provider(s) to use to test on
+#'   Linux, Windows, and macOS, respectively.
+#' @param deploy `[string]`\cr Which CI provider(s) to use to
+#'   deploy artifacts such as pkgdown documentation.
+#' @param matrix `[string]`\cr For which CI provider(s) to set up
+#'   matrix builds.
 #' @param quiet `[flag]`\cr Less verbose output? Default: `FALSE`.
 #' @export
-use_tic <- function(quiet = FALSE) { # nolint
+#' @examples
+#' # Requires interactive mode
+#' if (FALSE) {
+#'   use_tic()
+#'
+#'   # Pre-specified settings favoring Circle CI:
+#'   use_tic(
+#'     linux = "circle",
+#'     mac = "travis",
+#'     windows = "appveyor",
+#'     deploy = "circle",
+#'     matrix = "all"
+#'   )
+#' }
+use_tic <- function(wizard = interactive(),
+                    linux = c("travis", "circle", "none", "all"),
+                    mac = c("none", "travis", "all"),
+                    windows = c("none", "appveyor", "all"),
+                    deploy = c("travis", "circle", "none", "all"),
+                    matrix = c("none", "travis", "circle", "appveyor", "all"),
+                    quiet = FALSE) { # nolint
   cli_alert("Welcome to {.pkg tic}!")
   cli_text(
     "This wizard will guide you through the setup process for getting started
@@ -35,37 +64,62 @@ use_tic <- function(quiet = FALSE) { # nolint
   cli_end()
 
   cli_text("We recommend the following setup:")
-  cli_ul(c("Appveyor: Windows", "Circle CI: Linux", "Travis CI: macOS"))
+  cli_ul(c("Travis CI: Linux", "Travis CI: macOS", "Appveyor: Windows"))
   cli_par()
   cli_end()
 
-  if (yesno("Ready to get started?")) {
-    return(invisible(NULL))
+  if (wizard) {
+    if (yesno("Ready to get started?")) {
+      return(invisible(NULL))
+    }
+
+    cli_h1("Choosing your setup.")
+    cli_text("We'll ask you a few yes/no questions to gather your preferences.")
+    cli_par()
+    cli_end()
+
+    linux <- ci_menu(linux,
+      title = "Which provider do you want to use for Linux builds?"
+    )
+
+    mac <- ci_menu(mac,
+      title = "Do you want to build on macOS (= Travis CI)?"
+    )
+
+    windows <- ci_menu(windows,
+      title = "Do you want to build on Windows (= Appveyor)?"
+    )
+
+    deploy <- ci_menu(intersect(deploy, c(linux, mac, windows, "all", "none")),
+      title = "Do you want to deploy (i.e. push from the CI build to your repo) on certain providers? If yes, which ones?" # nolint
+    )
+
+    matrix <- ci_menu(intersect(matrix, c(linux, mac, windows, "all", "none")),
+      title = "Do you want to build on multiple R versions? (i.e. R-devel, R-release, R-oldrelease). If yes, on which platform(s)?" # nolint
+    )
+
+    wizard <- FALSE
+    use_tic_call <- c(
+      'tic::use_tic(',
+      arg_desc(wizard),
+      arg_desc(linux),
+      arg_desc(mac),
+      arg_desc(windows),
+      arg_desc(deploy),
+      arg_desc(matrix, last = TRUE),
+      ')'
+    )
+    cli_text("If setup fails, rerun with:")
+
+    # https://github.com/r-lib/cli/issues/127
+    withr::with_options(c(cli.width = 30), cli_code(use_tic_call))
+  } else {
+    linux <- match.arg(linux, several.ok = TRUE)
+    mac <- match.arg(mac, several.ok = TRUE)
+    windows <- match.arg(windows, several.ok = TRUE)
+    deploy <- match.arg(deploy, several.ok = TRUE)
+    matrix <- match.arg(matrix, several.ok = TRUE)
   }
-
-  cli_h1("Choosing your setup.")
-  cli_text("We'll ask you a few yes/no questions to gather your preferences.")
-  cli_par()
-  cli_end()
-
-  windows <- menu(c("Yes", "No"),
-    title = "Do you want to build on Windows (= Appveyor)?"
-  )
-  mac <- menu(c("Yes", "No"),
-    title = "Do you want to build on macOS (= Travis CI)?"
-  )
-
-  linux <- menu(c("Circle CI", "Travis CI", "None", "All"),
-    title = "Which provider do you want to use for Linux builds?"
-  )
-
-  deploy <- menu(c("Circle CI", "Travis CI", "No", "All"),
-    title = "Do you want to deploy (i.e. push from the CI build to your repo) on certain providers? If yes, which ones?" # nolint
-  )
-
-  matrix <- menu(c("Circle CI", "Travis CI", "Appveyor", "No", "All"),
-    title = "Do you want to build on multiple R versions? (i.e. R-devel, R-release, R-oldrelease). If yes, on which platform(s)?" # nolint
-  )
 
   cli_h1("Setting up the CI providers.")
 
@@ -79,26 +133,16 @@ use_tic <- function(quiet = FALSE) { # nolint
 
   # init deploy ----------------------------------------------------------------
 
-  if (deploy == 1) {
+  if ("circle" %in% deploy) {
     rule(left = "Circle CI")
     check_circle_pkg()
     circle::enable_repo()
     circle::use_circle_deploy()
-  } else if (deploy == 2) {
-    rule(left = "Travis CI")
-    travis::travis_enable()
-    check_travis_pkg()
-    travis::use_travis_deploy()
-  } else if (deploy == 3) {
+  } else if ("travis" %in% deploy) {
     rule(left = "Travis CI")
     check_travis_pkg()
     travis::travis_enable()
     travis::use_travis_deploy()
-
-    rule(left = "Circle CI")
-    check_circle_pkg()
-    circle::enable_repo()
-    circle::use_circle_deploy()
   }
 
   # create YAMLs ---------------------------------------------------------------
@@ -111,34 +155,40 @@ use_tic <- function(quiet = FALSE) { # nolint
 
   rule(left = "Travis CI")
 
-  if (linux == 2 || linux == 4) {
+  if (travis_in(linux)) {
     # deployment
-    if (deploy == 2 || deploy == 4) {
+    if (travis_in(deploy)) {
       # build matrix
-      if (matrix == 2 || matrix == 5) {
+      if (travis_in(matrix)) {
         use_travis_yml("linux-deploy-matrix")
       } else {
         use_travis_yml("linux-deploy")
       }
     } else {
       # build matrix
-      if (matrix == 2 || matrix == 5) {
+      if (travis_in(matrix)) {
         use_travis_yml("linux-matrix")
       } else {
         use_travis_yml("linux")
       }
     }
-  } else if (mac == 1) {
-    if (deploy == 2 || deploy == 4) {
+  }
+
+  if (travis_in(mac)) {
+    if (travis_in(linux)) {
+      stopc("Currently failing, https://github.com/ropenscilabs/tic/issues/202")
+    }
+
+    if (travis_in(deploy)) {
       # build matrix
-      if (matrix == 2 || matrix == 5) {
+      if (travis_in(matrix)) {
         use_travis_yml("macos-deploy-matrix")
       } else {
         use_travis_yml("macos-deploy")
       }
     } else {
       # build matrix
-      if (matrix == 2 || matrix == 5) {
+      if (travis_in(matrix)) {
         use_travis_yml("macos-matrix")
       } else {
         use_travis_yml("macos")
@@ -148,21 +198,20 @@ use_tic <- function(quiet = FALSE) { # nolint
 
   # Circle ---------------------------------------------------------------------
 
-  Sys.sleep(3)
   rule(left = "Circle CI")
 
-  if (linux == 1 || linux == 4) {
+  if (circle_in(linux)) {
     # deployment
-    if (deploy == 1 || deploy == 4) {
+    if (circle_in(deploy)) {
       # build matrix
-      if (matrix == 1 || matrix == 5) {
+      if (circle_in(matrix)) {
         use_circle_yml("linux-deploy-matrix")
       } else {
         use_circle_yml("linux-deploy")
       }
     } else {
       # build matrix
-      if (matrix == 1 || matrix == 5) {
+      if (circle_in(matrix)) {
         use_circle_yml("linux-matrix")
       } else {
         use_circle_yml("linux")
@@ -172,11 +221,10 @@ use_tic <- function(quiet = FALSE) { # nolint
 
   # Appveyor -------------------------------------------------------------------
 
-  Sys.sleep(3)
   rule(left = "Appveyor CI")
 
-  if (windows == 1) {
-    if (matrix == 3 || matrix == 5) {
+  if (appveyor_in(windows)) {
+    if (appveyor_in(matrix)) {
       use_appveyor_yml("windows-matrix")
     } else {
       use_appveyor_yml("windows")
@@ -185,7 +233,6 @@ use_tic <- function(quiet = FALSE) { # nolint
 
   # tic.R ----------------------------------------------------------------------
 
-  Sys.sleep(3)
   rule(left = "tic")
 
   use_tic_r(repo_type = detect_repo_type())
@@ -219,6 +266,38 @@ use_tic <- function(quiet = FALSE) { # nolint
   )
   tree(data, root = basename(getwd()))
 
+}
+
+arg_desc <- function(arg, last = FALSE) {
+  arg_name <- substitute(arg)
+  arg_value <- deparse(arg)
+  paste0("  ", arg_name, " = ", arg_value, if (!last) ",")
+}
+
+travis_in <- function(x) {
+  !all(is.na(match(c("travis", "all"), x)))
+}
+
+circle_in <- function(x) {
+  !all(is.na(match(c("circle", "all"), x)))
+}
+
+appveyor_in <- function(x) {
+  !all(is.na(match(c("appveyor", "all"), x)))
+}
+
+ci_menu <- function(choices, title) {
+  choice_map <- c(
+    travis = "Travis CI",
+    circle = "Circle CI",
+    appveyor = "AppVeyor CI",
+    all = "All",
+    none = "None"
+  )
+
+  reply <- menu(choice_map[choices], title = title)
+  stopifnot(reply != 0)
+  choices[reply]
 }
 
 use_tic_r <- function(repo_type) {
