e8f9d71ebab5a0d9f2d01837a7c598e0b4b618c5
pat-s <patrick.schratz@gmail.com>
2020-01-08 00:14:26 +0100

    file_exists -> file.exists

--- R/stage.R
+++ R/stage.R
@@ -36,7 +36,7 @@ TicStage <- R6Class( # nolint
 
       # check if DESCRIPTION file exists
       path <- paste0(usethis::proj_get(), "/DESCRIPTION")
-      if (!file_exists(path)) {
+      if (!file.exists(path)) {
         cli::cli_alert_danger("{.pkg tic} requires a DESCRIPTION file for
         installing dependencies, please add one.", wrap = TRUE)
         stop("Can't find DESCRIPTION.",
