c520ee4593668d8c886be3891c4094946413c75a
pat-s <patrick.schratz@gmail.com>
2020-01-24 14:10:28 +0100

    fix pkgType arg passing

--- R/install.R
+++ R/install.R
@@ -1,7 +1,7 @@
 # This code can only run as part of a CI run
 # nocov start
 
-verify_install <- function(pkg_names, pkgType = "both") { # nolint
+verify_install <- function(pkg_names, pkgType = NULL) { # nolint
   # set "type" to platform default
   pkgType <- update_type(pkgType) # nolint
   lapply(pkg_names, function(x) verify_install_one(x, pkgType = pkgType))
@@ -10,7 +10,8 @@ verify_install <- function(pkg_names, pkgType = "both") { # nolint
 verify_install_one <- function(pkg_name, pkgType) { # nolint
   withr::with_options(
     c(pkgType = pkgType),
-    remotes::install_cran(pkg_name, upgrade = TRUE)
+    #remotes::install_cran(pkg_name, upgrade = TRUE)
+    install.packages(pkg_name)
   )
   if (!package_installed(pkg_name)) {
     stopc(
