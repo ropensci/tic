393d2900304209952a78b0bbf754e41a351c2893
pat-s <patrick.schratz@gmail.com>
2020-02-19 08:44:39 +0100

    buildignore .github/

--- R/use-yaml.R
+++ R/use-yaml.R
@@ -276,7 +276,7 @@ use_ghactions_yml <- function(type = "all", deploy = FALSE) {
 
   # .ccache dir lives in the package root because we cannot write elsewhere
   # -> need to ignore it for R CMD check
-  usethis::use_build_ignore(".ccache")
+  usethis::use_build_ignore(c(".ccache", ".github"))
 
   if (type == "linux" | type == "macOS" | type == "linux-macos" |
     type == "linux-macos-windows" | type == "all") {
