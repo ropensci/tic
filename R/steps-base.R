419052e1e2dc1051a9960e0f8b8a288b5bc65fd3
pat-s <patrick.schratz@gmail.com>
2020-03-12 17:41:20 +0100

    document R6 class TicStep via roxygen notation

--- R/steps-base.R
+++ R/steps-base.R
@@ -1,49 +1,50 @@
-#' The base class for all steps
+#' @title The base class for all steps
 #'
+#' @description
 #' Override this class to create a new step.
 #'
 #' @importFrom R6 R6Class
 #' @export
 TicStep <- R6Class(
-  #' @section Methods:
-  #' \describe{
   "TicStep",
   cloneable = FALSE,
 
   public = list(
+
+    #' @description
+    #' Create a `TicStep` object.
     initialize = function() {
     },
 
+    #' @description
+    #' This method must be overridden, it is called when running the stage
+    #'   to which a step has been added.
     run = function() {
-      #' \item{`run`}{
-      #'   This method must be overridden, it is called when running the stage
-      #'   to which a step has been added.
-      #' }
       stopc("Please override the run() method to do something useful.")
     },
 
+    #' @description
+    #' This is just a placeholder.
+    #' This method is called when preparing the stage to
+    #' which a step has been added. It auto-install all packages which are
+    #' needed for a certain step. For example, `step_build_pkgdown()` requires
+    #' the _pkgdown_ package.
+    #'
+    #' For `add_code_step()`, it autodetects any package calls in the form of
+    #' `pkg::fun` and tries to install these packages from CRAN. If a steps
+    #' `prepare_call` is not empty, the `$prepare` method is skipped for this
+    #' step. This can be useful if a package should be installed from
+    #' non-standard repositories, e.g. from GitHub.
     prepare = function() {
-      #' \item{`prepare`}{ This method is called when preparing the stage to
-      #' which a step has been added. It auto-install all packages which are
-      #' needed for a certain step. For example, `step_build_pkgdown()` requires
-      #' the _pkgdown_ package.
-      #'
-      #' For `add_code_step()`, it autodetects any package calls in the form of
-      #' `pkg::fun` and tries to install these packages from CRAN. If a steps
-      #' `prepare_call` is not empty, the `$prepare` method is skipped for this
-      #' step. This can be useful if a package should be installed from
-      #' non-standard repositories, e.g. from GitHub. }
     },
 
+    #' @description
+    #' This method determines if a step is prepared and run.
+    #' Return `FALSE` if conditions for running this step are not met.
     check = function() {
-      #' \item{`check`}{
-      #'   This method determines if a step is prepared and run.
-      #'   Return `FALSE` if conditions for running this step are not met.
-      #' }
       TRUE
     }
   )
-  #' }
 )
 
 HelloWorld <- R6Class(
