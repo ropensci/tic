834e71a7a72503ff63787beb8957ecce96f2b627
pat-s <patrick.schratz@gmail.com>
2020-05-09 16:07:23 +0200

    fix tests

--- tests/testthat/test-update-yaml.R
+++ tests/testthat/test-update-yaml.R
@@ -1,34 +1,34 @@
 test_that("update_yml() preserves custom user blocks", {
   update_yml(
-    "inst/test-helpers/test-update-yaml.yml",
-    "inst/test-helpers/test-update-yaml-updated.yml"
+    system.file("testdata/test-update-yaml.yml", package = "tic"),
+    paste0(tempdir(), "test-update-yaml-updated.yml")
   )
 
-  updated <- readLines("inst/test-helpers/test-update-yaml-updated.yml")
-  solution <- readLines("inst/test-helpers/test-update-yaml-solution.yml")
+  updated <- readLines(paste0(tempdir(), "test-update-yaml-updated.yml"))
+  solution <- readLines(system.file("testdata/test-update-yaml-solution.yml", package = "tic")) # nolint
   expect_equal(updated, solution)
 })
 
 test_that("update_yml() preserves custom env vars", {
   update_yml(
-    "inst/test-helpers/test-update-yaml-env-var.yml",
-    "inst/test-helpers/test-update-yaml-env-var-updated.yml"
+    system.file("testdata/test-update-yaml-env-var.yml", package = "tic"),
+    paste0(tempdir(), "test-update-yaml-env-var-updated.yml")
   )
 
-  updated <- readLines("inst/test-helpers/test-update-yaml-env-var-updated.yml")
-  solution <- readLines("inst/test-helpers/test-update-yaml-env-var-solution.yml") # nolint
+  updated <-readLines(paste0(tempdir(), "test-update-yaml-env-var-updated.yml")) # nolint
+  solution <- readLines(system.file("testdata/test-update-yaml-env-var-solution.yml", package = "tic")) # nolint
   expect_equal(updated, solution)
 })
 
 
 test_that("update_yml() preserves custom env vars AND blocks", {
   update_yml(
-    "inst/test-helpers/test-update-yaml-env-and-blocks.yml",
-    "inst/test-helpers/test-update-yaml-env-and-blocks-updated.yml"
+    system.file("testdata/test-update-yaml-env-and-blocks.yml", package = "tic"),
+    paste0(tempdir(), "test-update-yaml-env-and-blocks-updated.yml")
   )
 
-  updated <- readLines("inst/test-helpers/test-update-yaml-env-and-blocks-updated.yml") # nolint
-  solution <- readLines("inst/test-helpers/test-update-yaml-env-and-blocks-solution.yml") # nolint
+  updated <- readLines(paste0(tempdir(), "test-update-yaml-env-and-blocks-updated.yml")) # nolint
+  solution <- readLines(system.file("testdata/test-update-yaml-env-and-blocks-solution.yml", package = "tic")) # nolint
   expect_equal(updated, solution)
 })
 
