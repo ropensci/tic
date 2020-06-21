c26db64b0ab93e5e11198ec1e09ede72744ef70b
pat-s <patrick.schratz@gmail.com>
2020-06-21 11:33:49 +0200

    fix na.omit NOTE in R CMD check

--- R/update-yaml-helpers.R
+++ R/update-yaml-helpers.R
@@ -2,6 +2,8 @@ account_for_dup_env_vars <- function(custom_env_var_list,
                                      env_var_index_latest,
                                      tmpl_latest) {
 
+  requireNamespace("stats", quietly = TRUE)
+
   # get all custom env vars
   env_vars_raw <- gsub(" ", "",
     unlist(
@@ -28,7 +30,7 @@ account_for_dup_env_vars <- function(custom_env_var_list,
         grep(env_vars_raw[i], tmpl_latest)[2]
       )
       tmpl_latest[env_to_remove] <- NA
-      tmpl_latest <- na.omit(tmpl_latest)
+      tmpl_latest <- stats::na.omit(tmpl_latest)
       cli::cli_alert_info("Found duplicated env var {.var {env_vars_raw[i]}}
           after updating to the latest template version.
           Removed the latest instance to avoid duplicates.
