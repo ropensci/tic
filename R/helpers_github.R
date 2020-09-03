ae2c94a6f3cc622135254020b6fb672b36aaff3a
pat-s <patrick.schratz@gmail.com>
2020-09-03 08:45:00 +0200

    add extract_repo() and get_remote_url()

--- R/helpers_github.R
+++ R/helpers_github.R
@@ -161,6 +161,34 @@ get_repo_data <- function(repo) {
   return(req)
 }
 
+get_remote_url <- function(path) {
+  r <- git2r::repository(path, discover = TRUE)
+  remote_names <- git2r::remotes(r)
+  if (!length(remote_names)) {
+    stop("Failed to lookup git remotes") # nocov
+  }
+  remote_name <- "origin"
+  if (!("origin" %in% remote_names)) { # nocov start
+    remote_name <- remote_names[1]
+    warning(sprintf("No remote 'origin' found. Using: %s", remote_name))
+  } # nocov end
+  git2r::remote_url(r, remote_name)
+}
+
+extract_repo <- function(url) {
+  # Borrowed from gh:::github_remote_parse
+  re <- "github[^/:]*[/:]([^/]+)/(.*?)(?:\\.git)?$"
+  m <- regexec(re, url)
+  match <- regmatches(url, m)[[1]]
+
+  if (length(match) == 0) {
+    stop("Unrecognized repo format: ", url) # nocov
+  }
+
+  paste0(match[2], "/", match[3])
+}
+
+
 # GH auth helpers --------------------------------------------------------------
 
 #' @title Github API helpers
